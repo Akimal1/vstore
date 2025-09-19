17d1d0af7e8c0141c5c270e10e1dc5a3e7889c2d
ak1movumar <akimovumar2005@gmail.com>
2025-09-19 19:01:53 +0600

    added crud

--- src/routes/Mainroutes.jsx
+++ src/routes/Mainroutes.jsx
@@ -3,12 +3,14 @@ import { Route, Routes } from "react-router-dom";
 import Catalog from "../pages/catalog/Catalog";
 import Home from "../pages/home/Home";
 import Admin from "../pages/admin/Admin";
+import UpdatePage from "../pages/update/UpdatePage";
 
 const MainRoutes = () => {
   const routes = [
     { link: "/", element: <Home /> },
     { link: "*", element: "" },
     { link: "/admin", element: <Admin /> },
+    { link: "/update/:id", element: <UpdatePage /> },
   ];
   return (
     <Routes>
